2abb290fff279926fde01b3e5ff55a73fc6005cf
colenocks <enockscoleman@gmail.com>
2020-05-13 21:40:36 +0100

    feat: install axious and create listing component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,30 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import PlayerList from "./Player/PlayerList";
 import PlayerForm from "./Player/PlayerForm";
 import PlayerSingle from "./Player/PlayerSingle";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">Menu</div>
-      </div>
-      <div className="row">
-        <div className="col s3">
-          <PlayerList />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {},
+    };
+
+    //bind the function so to use in a class
+    this.updateCurrentPlayer = this.updateCurrentPlayer.bind(this);
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:4000/players";
+
+    axios
+      .get(url)
+      .then((Response) => {
+        this.setState({
+          players: Response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  updateCurrentPlayer(item) {
+    this.setState({
+      currentPlayer: item,
+    });
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col s12">Menu</div>
         </div>
-        <div className="col s9">
-          <PlayerSingle />
+        <div className="row">
+          <div className="col s3">
+            <PlayerList
+              players={this.state.players}
+              updateCurrentPlayer={this.updateCurrentPlayer}
+            />
+          </div>
+          <div className="col s9">
+            <PlayerSingle />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col s12">
-          <PlayerForm />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/Player/PlayerList.jsx
+++ frontend/src/components/Player/PlayerList.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
-const PlayerList = () => {
-  return <div>Player List</div>;
+const PlayerList = (props) => {
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4>Players</h4>
+        </li>
+        {props.players.map((item) => (
+          <a
+            href="#!"
+            className="collection-item"
+            key={item._id}
+            onClick={() => props.updateCurrentPlayer}
+          >
+            {item.firstName} {item.lastName}
+          </a>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PlayerList;
