8896023105efe04ed2da7a9d4815e257bea0212c
colenocks <enockscoleman@gmail.com>
2020-05-13 19:13:23 +0100

    feat: add style library and folder structure

---
+++ frontend/src/components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import PlayerList from "./Player/PlayerList";
+import PlayerForm from "./Player/PlayerForm";
+import PlayerSingle from "./Player/PlayerSingle";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col s12">Menu</div>
+      </div>
+      <div className="row">
+        <div className="col s3">
+          <PlayerList />
+        </div>
+        <div className="col s9">
+          <PlayerSingle />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <PlayerForm />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Player/PlayerForm.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class PlayerForm extends Component {
+  state = {};
+  render() {
+    return <div>Player Form</div>;
+  }
+}
+
+export default PlayerForm;
